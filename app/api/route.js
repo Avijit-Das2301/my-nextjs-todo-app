3ac0eb09cb9d6323d8a9db44144e31918b5d4caa
Avijit Das <avijitdas2301@gmail.com>
2025-09-20 21:50:37 +0530

    Initial commit for Todo App,fixed credentials

---
+++ app/api/route.js
@@ -0,0 +1,41 @@
+import { connectDB } from "@/lib/config/db";
+import TodoModel from "@/lib/config/models/TodoModel";
+import { isCancel } from "axios";
+import { connect } from "mongoose";
+import { NextResponse } from "next/server";
+
+const LoadDB = async () => {
+  await connectDB();
+}
+
+LoadDB();
+
+export async function GET(request) {
+  const todos = await TodoModel.find({});
+  return NextResponse.json({todos:todos})
+}
+
+export async function POST(request) {
+  const {title,description} = await request.json();
+  await TodoModel.create({
+    title,
+    description
+  })
+  return NextResponse.json({msg:"Todo Created"})
+}
+
+export async function DELETE(request) {
+  const mongoId = await request.nextUrl.searchParams.get('mongoId');
+  await TodoModel.findByIdAndDelete(mongoId);
+  return NextResponse.json({msg:"Todo Deleted"})
+}
+
+export async function PUT(request) {
+  const mongoId = await request.nextUrl.searchParams.get('mongoId');
+  await TodoModel.findByIdAndUpdate(mongoId,{
+    $set:{
+      isCompleted:true
+    }
+  });
+  return NextResponse.json({msg:"Todo Completed"})
+}
